94f7ed3965095625bb72f324cff1cb1b21caafe6
unknown <ajaydevofficial@gmail.com>
2020-02-01 13:19:59 +0530

    Donor Check

--- frontend/src/app/usercards/usercards.component.ts
+++ frontend/src/app/usercards/usercards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase'
 
 @Component({
   selector: 'app-usercards',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsercardsComponent implements OnInit {
 
+  loaded:boolean = false;
+  isDonorRegistered:boolean;
+  uid = JSON.parse(localStorage.getItem('loggedUser')).uid
+
   constructor() { }
 
   ngOnInit() {
+    firebase.database().ref('donors/' + this.uid).on('value',(snap)=>{
+        this.isDonorRegistered = snap.exists();
+        this.loaded = true;
+    });
   }
+
+
 
 }
