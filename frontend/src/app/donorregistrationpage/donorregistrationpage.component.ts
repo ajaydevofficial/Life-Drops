ed0089c913ca07764eb7405df297450c37d1050c
unknown <ajaydevofficial@gmail.com>
2020-02-01 13:19:44 +0530

    Donor Registration

--- frontend/src/app/donorregistrationpage/donorregistrationpage.component.ts
+++ frontend/src/app/donorregistrationpage/donorregistrationpage.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+import { NotifierService } from '../shared/notifier.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-donorregistrationpage',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonorregistrationpageComponent implements OnInit {
 
-  constructor() { }
+  disabledValue:boolean = true;
+
+  get userName(){
+    return JSON.parse(localStorage.getItem('loggedUser')).displayName
+  }
+  get userEmail(){
+    return JSON.parse(localStorage.getItem('loggedUser')).email
+  }
+  uid = JSON.parse(localStorage.getItem('loggedUser')).uid;
+  constructor(private notifier:NotifierService,private router:Router) { }
 
   ngOnInit() {
+  }
+
+  registerDonor(dob,bloodgroup,phone,city){
+    firebase.database().ref('donors/' + this.uid).once('value',(snap)=>{
+      if(!snap.exists()){
+        firebase.database().ref('donors/' + this.uid ).push({
+          full_name : this.userName,
+          email : this.userEmail,
+          date_of_birth  : dob,
+          blood_group : bloodgroup,
+          phone : phone,
+          ciry : city,
+          uid : this.uid,
+          status : 'Can Donate'
+        }).then(()=>{
+          this.notifier.display('success','You Have Succesfully Registered as Donor')
+          this.router.navigate(['/'])
+        }).catch(error=>{
+          this.notifier.display('error','Oops! Something went wrong, please try again')
+        }) 
+      }
+      else{
+        this.notifier.display('error','User already registered')
+      }
+    })
   }
 
 }
