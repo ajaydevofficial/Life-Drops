7bf286d465ff8f4d7794fd8923798e5c8d963117
unknown <ajaydevofficial@gmail.com>
2020-02-03 00:18:16 +0530

    Donation Event Page Phase 1

--- frontend/src/app/donationeventpage/donationeventpage.component.ts
+++ frontend/src/app/donationeventpage/donationeventpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-donationeventpage',
@@ -7,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DonationeventpageComponent implements OnInit {
 
-  donationEventList = [1,2,3,4,5,6,7,8,9]
+  donationEventList;
+  currentDate; 
   constructor() { }
 
   ngOnInit() {
+    this.currentDate = this.formatDate(new Date())
+    window.moveTo(0,0);
+    firebase.database().ref('blood-donation-events').on('value',(snap)=>{
+      this.donationEventList = []
+      snap.forEach(element=>{
+        var value = element.val()
+        value['directionLink'] = "https://www.google.com/maps/place/" + element.val().location.lat + ',' + element.val().location.lng;
+        this.donationEventList.push(value);
+      })
+    })
   }
+
+  formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
 
 }
