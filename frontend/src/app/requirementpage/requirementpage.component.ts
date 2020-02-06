24247d2783828c3071bc83575870b1fe3a665a87
unknown <ajaydevofficial@gmail.com>
2020-02-06 14:35:29 +0530

    Requirements Page

--- frontend/src/app/requirementpage/requirementpage.component.ts
+++ frontend/src/app/requirementpage/requirementpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-requirementpage',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requirementpage.component.css']
 })
 export class RequirementpageComponent implements OnInit {
-  requirementsList = [1,2,3,4,5,6,7,8,9,10,11,12,12,14,15];
+  requirementsList;
   constructor() { }
 
   ngOnInit() {
+    window.moveTo(0,0);
+    firebase.database().ref('blood-requirments').on('value',(snap)=>{
+      this.requirementsList = []
+      snap.forEach(element=>{
+        var value = element.val()
+        value['directionLink'] = "https://www.google.com/maps/place/" + element.val().location.lat + ',' + element.val().location.lng;
+        this.requirementsList.push(value);
+      })
+      console.log(this.requirementsList)
+    })
+    
   }
 
 }
