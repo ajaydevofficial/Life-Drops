8e3a530d13d76913191067c06ed8b635da36d086
unknown <ajaydevofficial@gmail.com>
2020-02-10 11:42:13 +0530

    Display Count in User Cards

--- frontend/src/app/usercards/usercards.component.ts
+++ frontend/src/app/usercards/usercards.component.ts
@@ -11,10 +11,22 @@ export class UsercardsComponent implements OnInit {
   loaded:boolean = false;
   isDonorRegistered:boolean;
   uid = JSON.parse(localStorage.getItem('loggedUser')).uid
+  eventCount;
+  urgentRequirementCount;
+  requirementCount;
 
   constructor() { }
 
   ngOnInit() {
+    firebase.database().ref('blood-donation-events/').on('value',(snap)=>{
+      this.eventCount = snap.numChildren()
+    });
+    firebase.database().ref('urgent-requirments/').on('value',(snap)=>{
+      this.urgentRequirementCount = snap.numChildren()
+    });
+    firebase.database().ref('blood-requirments/').on('value',(snap)=>{
+      this.requirementCount = snap.numChildren()
+    });
     firebase.database().ref('donors/' + this.uid).on('value',(snap)=>{
         this.isDonorRegistered = snap.exists();
         this.loaded = true;
