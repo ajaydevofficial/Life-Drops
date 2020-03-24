f41ca9d990046668cb4b81f67b41708f9d7b59e6
unknown <ajaydevofficial@gmail.com>
2020-03-25 00:40:16 +0530

    Urgent requirements admin

--- frontend/src/app/admin/urgent-requirement/urgent-requirement.component.ts
+++ frontend/src/app/admin/urgent-requirement/urgent-requirement.component.ts
@@ -1,4 +1,6 @@
+import { ExportAsConfig, ExportAsService } from 'ngx-export-as';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-urgent-requirement',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UrgentRequirementComponent implements OnInit {
 
-  constructor() { }
+  requirementsList;
+  searchText;
+  page;
+
+  closedrequirementsList;
+  closedSearchText;
+  closedPage
+
+  constructor(private exportAsService:ExportAsService) { }
+
+  exportAsConfig: ExportAsConfig = {
+    type: 'xlsx',
+    elementId: 'table',
+  }
+  exportAsConfigClosed: ExportAsConfig = {
+    type: 'xlsx',
+    elementId: 'closedtable',
+  }
 
   ngOnInit() {
+    firebase.database().ref('urgent-requirments').on('value',(snap)=>{
+      this.requirementsList = [];
+      this.closedrequirementsList = [];
+      snap.forEach(element=>{
+        var value = element.val();
+        value['key'] = element.key;
+        if(value.status == 'open'){
+          this.requirementsList.push(value);
+        }
+        else{
+          this.closedrequirementsList.push(value);
+        }
+      })
+    })
+  }
+
+  export() {
+    this.exportAsService.save(this.exportAsConfig, 'urgent requirements').subscribe(() => {});
+  }
+
+  closedExport() {
+    this.exportAsService.save(this.exportAsConfigClosed, 'urgent requirements-closed').subscribe(() => {});
+  }
+
+  close(key){
+    firebase.database().ref('urgent-requirments/' + key).update({
+      status: "closed",
+      verification: "Verified"
+    });
+  }
+
+  delete(key){
+    firebase.database().ref('urgent-requirments/' + key).remove();
   }
 
 }
