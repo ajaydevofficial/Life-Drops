4cf6a91f286ee1cec10b39de90716ee987504a45
unknown <ajaydevofficial@gmail.com>
2020-03-26 21:08:19 +0530

    Card Changes

--- frontend/src/app/admin/cards/cards.component.ts
+++ frontend/src/app/admin/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-cards',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsComponent implements OnInit {
 
+  eventCount:number;
+  urgentRequirementCount:number;
+  requirementCount:number;
+  loaded:boolean = false;
+
   constructor() { }
 
   ngOnInit() {
+    firebase.database().ref('blood-donation-events/').on('value',(snap)=>{
+      this.eventCount = snap.numChildren()
+    });
+    firebase.database().ref('urgent-requirments/').on('value',(snap)=>{
+      this.urgentRequirementCount = snap.numChildren()
+    });
+    firebase.database().ref('blood-requirments/').on('value',(snap)=>{
+      this.requirementCount = snap.numChildren()
+      this.loaded = true;
+    })
   }
 
 }
